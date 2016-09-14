10bd97a9aba8891a8f25a7e235fb16dff7577d47
Jason Ryan <jason-ryan@uiowa.edu>
2016-09-14 16:14:15 -0500

    Added constructor to ArabicToRomanFrame.

--- oral_exam1/S15_ArabicToRomanGUI/src/ArabicToRomanDriver.java
+++ oral_exam1/S15_ArabicToRomanGUI/src/ArabicToRomanDriver.java
@@ -1,8 +1,13 @@
+import javax.swing.*;
+
 /**
  * Created by jasonryan on 9/14/16.
  */
 public class ArabicToRomanDriver {
     public static void main(String args[]) {
-
+        ArabicToRomanFrame frame = new ArabicToRomanFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 100);
+        frame.setVisible(true);
     }
 }
--- oral_exam1/S15_ArabicToRomanGUI/src/ArabicToRomanFrame.java
+++ oral_exam1/S15_ArabicToRomanGUI/src/ArabicToRomanFrame.java
@@ -6,4 +6,26 @@ import java.awt.*;
  */
 public class ArabicToRomanFrame extends JFrame {
 
+    // Constants for number of columns required in text fields.
+    private static final int MAX_ROMAN = 9;
+    private static final int MAX_ARABIC = 4;
+
+    public ArabicToRomanFrame() {
+        super("S15_ArabicToRomanGUI");
+        setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        JTextField romanField = new JTextField(MAX_ROMAN);
+        JTextField ArabicField = new JTextField(MAX_ARABIC);
+
+        JLabel romanLabel = new JLabel("Roman Numerals: ");
+        JLabel arabicLabel = new JLabel("Arabic Digits: ");
+
+        romanLabel.setPreferredSize(new Dimension(150,20));
+        arabicLabel.setPreferredSize(new Dimension(150,20));
+
+        add(romanLabel);
+        add(romanField);
+        add(arabicLabel);
+        add(ArabicField);
+    }
 }
