d3420ccc112d78e939f1a63bb09ae669a6135398
Jason Ryan <jason-ryan@uiowa.edu>
2016-10-03 15:10:21 -0500

    Added constructor and toString() to BaseStudent class

--- oral_exam1/S23_StudentTuition/src/BaseStudent.java
+++ oral_exam1/S23_StudentTuition/src/BaseStudent.java
@@ -2,4 +2,20 @@
  * Created by jasonryan on 10/3/16.
  */
 public class BaseStudent {
+    int baseClassHourTuitionRate = 350;
+    String firstName, lastName;
+    int studentID, numberOfRegisteredHours;
+
+    // Constructor
+    public BaseStudent(String first, String last, int id, int hours) {
+        firstName = first;
+        lastName = last;
+        studentID = id;
+        numberOfRegisteredHours = hours;
+    }
+
+    @Override
+    public String toString() {
+        return "First Name: " + firstName + "/nLast Name: " + lastName + "\nStudent ID: " + studentID + "\nHours Registered: " + numberOfRegisteredHours;
+    }
 }
--- oral_exam1/S23_StudentTuition/src/StudentTuitionDriver.java
+++ oral_exam1/S23_StudentTuition/src/StudentTuitionDriver.java
@@ -2,4 +2,7 @@
  * Created by jasonryan on 10/3/16.
  */
 public class StudentTuitionDriver {
+    public static void main(String[] args) {
+
+    }
 }
