c426878eea2d4ef6cbafd82a84915268ba3de654
Jason Ryan <jason-ryan@uiowa.edu>
2016-10-03 15:13:39 -0500

    Added getters and setters.

--- oral_exam1/S23_StudentTuition/src/BaseStudent.java
+++ oral_exam1/S23_StudentTuition/src/BaseStudent.java
@@ -14,6 +14,46 @@ public class BaseStudent {
         numberOfRegisteredHours = hours;
     }
 
+    public int getBaseClassHourTuitionRate() {
+        return baseClassHourTuitionRate;
+    }
+
+    public void setBaseClassHourTuitionRate(int baseClassHourTuitionRate) {
+        this.baseClassHourTuitionRate = baseClassHourTuitionRate;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(int studentID) {
+        this.studentID = studentID;
+    }
+
+    public int getNumberOfRegisteredHours() {
+        return numberOfRegisteredHours;
+    }
+
+    public void setNumberOfRegisteredHours(int numberOfRegisteredHours) {
+        this.numberOfRegisteredHours = numberOfRegisteredHours;
+    }
+
     @Override
     public String toString() {
         return "First Name: " + firstName + "/nLast Name: " + lastName + "\nStudent ID: " + studentID + "\nHours Registered: " + numberOfRegisteredHours;
