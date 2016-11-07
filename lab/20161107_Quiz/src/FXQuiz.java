eed41899efbcc5c141b80310f562d16539d83e1a
Jason Ryan <jason-ryan@uiowa.edu>
2016-11-07 12:21:53 -0600

    20161107_Quiz created. Could not get Scene Builder installed to comoplete the quiz.

---
+++ lab/20161107_Quiz/src/FXQuiz.java
@@ -0,0 +1,18 @@
+/**
+ * Created by jasonryan on 11/7/16.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXQuiz extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
