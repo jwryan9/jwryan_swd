bcdcad24b31507fea6d6783ea1002a8d5002770e
Jason Ryan <jason-ryan@uiowa.edu>
2016-11-15 19:51:32 -0600

    Began implementation of merge sort. Problem with merge function.

---
+++ oral_exam2/S18_MergeSort_Medium/src/MergeSortDriver.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Driver program to show the capabilities of MergeSort program
+ *
+ * @author Jason Ryan
+ */
+public class MergeSortDriver {
+    private static final int ARR_LEN = 6;
+
+    public static void main(String[] args) {
+        int[] arr = new int[ARR_LEN];
+        Random random = new Random();
+
+        for(int i=0; i<ARR_LEN; i++) {
+            arr[i] = random.nextInt(1001);
+        }
+
+        System.out.println("Original: " + Arrays.toString(arr));
+        MergeSort.sort(arr, 0, ARR_LEN - 1);
+        System.out.println("Sorted: " + Arrays.toString(arr));
+    }
+}
