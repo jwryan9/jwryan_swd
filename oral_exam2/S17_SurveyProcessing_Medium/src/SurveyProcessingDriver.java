19b1002b56a6b5d6515bac1e94472329e14a2adc
Jason Ryan <jason-ryan@uiowa.edu>
2016-11-26 18:26:51 -0600

    Added user input capabilities.

--- oral_exam2/S17_SurveyProcessing_Medium/src/SurveyProcessingDriver.java
+++ oral_exam2/S17_SurveyProcessing_Medium/src/SurveyProcessingDriver.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 /**
  * Driver program for demonstrating the capabilities of survey processing program.
  *
  * @author Jason Ryan
  */
 public class SurveyProcessingDriver {
+
     public static void main(String[] args)
     {
+        String product = "", filter = "", inputPath = "", outputPath = "";
+        Scanner kb = new Scanner(System.in);
 
+        System.out.println("Enter filepath for input file:");
+        inputPath = kb.nextLine();
+        System.out.println("Enter product to filter on:");
+        product = kb.nextLine();
+        System.out.println("enter a demographic attribute to filter on. Enter a value contained in any of the following columns [Gender|Age Group|State of Residence]:");
+        filter = kb.nextLine();
+        System.out.println("Enter filepath for output file:");
+        outputPath = kb.nextLine();
+
+        SurveyParser surveyParser = new SurveyParser(inputPath);
+        surveyParser.parseData(product, filter);
+
+        ResultWriter resultWriter = new ResultWriter(outputPath);
+        resultWriter.writeFile(surveyParser.getRatings());
     }
 }
